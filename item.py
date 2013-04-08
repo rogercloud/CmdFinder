efa84812e714f2e466918fedf5d6f95730323821
Jicheng Shi <rogershijicheng@gmail.com>
2013-04-08 18:43:09 +0800

    create desc class to desription

--- item.py
+++ item.py
@@ -1,4 +1,5 @@
-class Item(object):
+class Entry(object):
+    ''' '''
     def __init__(self,summary,cmd,desc,vote):
         self.summary = summary
         self.cmd = cmd 
@@ -6,6 +7,29 @@ class Item(object):
         self.vote = vote
         
     def __str__(self):
-        d = '\n'.join(self.desc)
         return 'smmary: %s\ncmd: %s\ndesc: %s\nvote:%d\n' % (\
-                self.summary, self.cmd, d, self.vote)
+                self.summary, self.cmd, self.desc, self.vote)
+
+
+class Desc(object):
+    ''' Single description may contains multipul paragraphs '''
+    def __init__(self, desc):
+        self.desc = desc
+        self.desc_list = self._filter()
+        
+    def _filter(self):
+        perdes_str = []
+        # contents may contains '\n', ignore it
+        for para in [p for p in self.desc.contents if not isinstance(p,unicode)]:
+            pstr = ''
+            if para.name == 'p' or para.name == 'code':
+                for content in para.contents:
+                    if isinstance(content, unicode):
+                        pstr += content.strip()
+                    else:
+                        pstr += content.string.strip()
+                perdes_str.append(pstr)
+        return perdes_str
+
+    def __str__(self):
+        return '\n'.join(self.desc_list)
--- webcrawl.py
+++ webcrawl.py
@@ -1,37 +1,23 @@
 import urllib
 from bs4 import BeautifulSoup as BS
-from item import Item
+from item import Entry, Desc
+import sqlite3
 
-class Crawler():
+class Crawler(object):
     def __init__(self, url):
         page = urllib.urlopen(url).readlines()
-        self.soup = BS(''.join(page))
+        self._soup = BS(''.join(page))
+        self.entrys = []
 
     def filter(self):
         #summary
-        sums = [s.a.string for s in self.soup.find_all('div', 'summary')]
+        sums = [s.a.string for s in self._soup.find_all('div', 'summary')]
         #command
-        cmds = [c.string for c in self.soup.find_all('div', 'command')]
+        cmds = [c.string for c in self._soup.find_all('div', 'command')]
         #description
-        desc = self.soup.find_all('div', 'description')
-        desc_str = []
-        for perdes in desc:
-            perdes_str = []
-            for para in [p for p in perdes.contents if not isinstance(p,unicode)]:
-                pstr = ''
-                if para.name == 'p' or para.name == 'code':
-                    for content in para.contents:
-                        if isinstance(content, unicode):
-                            pstr += content
-                        else:
-                            pstr += content.string
-                    perdes_str.append(pstr)
-            desc_str.append(perdes_str)
-        print desc_str
+        desc = [Desc(per) for per in self._soup.find_all('div', 'description')]
         #vote
-        vote = self.soup.find_all('div', 'num-votes')
-        vote = [int(v.string) for v in vote]
+        vote = [int(v.string) for v in self._soup.find_all('div', 'num-votes')]
 
         for i in xrange(len(sums)):
-            it = Item(sums[i], cmds[i], desc_str[i], vote[i])
-            print it
+            self.entrys.append(Entry(sums[i], cmds[i], desc[i], vote[i]))
